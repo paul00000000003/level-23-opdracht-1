9e9574003db5c93592b9bfdce11a0a0ae3c55a28
Paul van Mierlo <pauljozefvanmierlo@yahoo.com>
2020-10-31 16:12:27 +0100

    rest en spread oparators

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
+const optellen = (...nums) => nums.reduce((som, element) => som + element, 0);
 
+const optellen2 = (num1, num2, num3, num4, num5, num6) => (num1 + num2 + num3 + num4 + num5 + num6);
+
+
+console.log(optellen(1, 2, 3, 4, 5, 6));
+console.log(optellen2(1, 2, 3, 4, 5, 6));
